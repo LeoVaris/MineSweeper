0bfa2056e1ddabf9601a654327469f4c8f74dac8
AseClan <43111991+AseClan@users.noreply.github.com>
2019-11-15 15:42:07 +0200

    Update

---
+++ src/MineSweeper/Grid.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+export default class Grid extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [],
+      grid_width = this.props.width,
+      grid_height = this.props.height,
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    const {grid_width, grid_height} = this.state
+    grid = CreateGrid(grid_width, grid_height);
+    this.setState({grid, loading: false});
+  }
+
+  render() {
+
+  }
+}
+
+const CreateGrid = (width, height) => {
+  grid = []
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+
+    }
+  }
+}
