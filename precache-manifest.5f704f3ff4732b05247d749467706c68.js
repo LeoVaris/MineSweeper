ec14b050b69fc41235c79283b293a1a770d19f3b
Leo Varis <leovaris04@gmail.com>
2019-11-24 16:34:42 +0200

    Updates

---
+++ precache-manifest.5f704f3ff4732b05247d749467706c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143a16b4fddcc226e10fb06a97d75ded",
+    "url": "/MineSweeper/index.html"
+  },
+  {
+    "revision": "4f45b256f5d1d49fdcc6",
+    "url": "/MineSweeper/static/css/main.ec575404.chunk.css"
+  },
+  {
+    "revision": "cfc2012169fd880bc767",
+    "url": "/MineSweeper/static/js/2.149c90e3.chunk.js"
+  },
+  {
+    "revision": "4f45b256f5d1d49fdcc6",
+    "url": "/MineSweeper/static/js/main.1b5b3578.chunk.js"
+  },
+  {
+    "revision": "574dd736de179ded0040",
+    "url": "/MineSweeper/static/js/runtime-main.9b5d6a1f.js"
+  }
+]);
