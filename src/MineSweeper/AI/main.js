ede7a204631d638ab772f44a8e7ed2f86696f449
Leo Varis <leovaris04@gmail.com>
2019-11-24 16:03:47 +0200

    Added comments

--- src/MineSweeper/AI/main.js
+++ src/MineSweeper/AI/main.js
@@ -4,6 +4,7 @@ import {simpleBombUpdate, simpleSafeUpdate, checkLinks,
 
 
 export default function AIMove(grid, mineCount, firstClick) {
+  // Just guess the first click
   if (firstClick) {
     const row = Math.floor(grid.length / 2)
     const col = Math.floor(grid[0].length / 2)
@@ -16,7 +17,9 @@ export default function AIMove(grid, mineCount, firstClick) {
     !node.isFlag &&
     node.bombsAround > 0
   );
+  // Check links 
   checkLinks(grid);
+  // Check simple logics
   toCheck.forEach(node => {
     const row = node.row;
     const col = node.col;
@@ -29,14 +32,16 @@ export default function AIMove(grid, mineCount, firstClick) {
   const bombs = hidden.filter(node => node.risk === 100);
   if (bombs.length > 0) return ({row: bombs[0].row, col: bombs[0].col, left: false});
   toCheck.forEach(node => {
+    // Do the risk calculation for these nodes
     complicatedRiskUpdate(grid, node.row, node.col);
   })
   let rest = hidden.filter(node => node.risk !== null);
   rest = sortByRisk(rest);
+  // if theres filtered options
   if (typeof rest[0] !== 'undefined') {
-    console.log("rest");
     return ({row: rest[0].row, col: rest[0].col, left: true});
   }
+  // Basicly a pure guess 
   const lowRisk = toCheck.filter(node => node.bombsAround === 1);
   let ret1 = [];
   lowRisk.forEach(node => {
